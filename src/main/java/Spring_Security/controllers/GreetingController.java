c83b2483622167df2d92edeea017832e71fc0eae
Udita2225 <usasinghudita2225@gmail.com>
2026-04-04 16:50:04 +0530

    4 April 2026

---
+++ src/main/java/Spring_Security/controllers/GreetingController.java
@@ -0,0 +1,12 @@
+package Spring_Security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+}
