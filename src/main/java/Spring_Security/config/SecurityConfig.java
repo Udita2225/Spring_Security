e23aa6c0a64ed40a375631839507445e3b168551
Udita2225 <usasinghudita2225@gmail.com>
2026-04-06 16:09:06 +0530

    6 April 2026

--- src/main/java/Spring_Security/config/SecurityConfig.java
+++ src/main/java/Spring_Security/config/SecurityConfig.java
@@ -1,4 +1,58 @@
 package Spring_Security.config;
 
+
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http){
+        http
+                .authorizeHttpRequests((requests) ->
+                                 requests
+                                .anyRequest()
+                                .authenticated());
+        http
+                .sessionManagement((session)->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 =  User.withUsername("user1")
+                .password("{noop}password1")
+                .roles("USER")
+                .build();
+        UserDetails admin =  User.withUsername("admin")
+                .password("{noop}password2")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
 }
