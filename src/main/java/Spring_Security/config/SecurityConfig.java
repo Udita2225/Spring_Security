08c5ee626fa015ea4005ea8fb096e78825cbd4a7
Udita2225 <usasinghudita2225@gmail.com>
2026-05-09 18:45:45 +0530

    9 My 2026

--- src/main/java/Spring_Security/config/SecurityConfig.java
+++ src/main/java/Spring_Security/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package Spring_Security.config;
 
 
+import Spring_Security.jwt.AuthEntryPointJwt;
+import Spring_Security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpRequest;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +27,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -35,11 +41,21 @@ public class SecurityConfig {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
     @Bean
     @Order(SecurityFilterProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http){
-        http.authorizeHttpRequests((requests)->
-                requests.requestMatchers("/h2-console/**")
+        http.authorizeHttpRequests((authorizeRequests)->
+                authorizeRequests.requestMatchers("/h2-console/**")
+                        .permitAll()
+                        .requestMatchers("/signin")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
@@ -61,34 +77,78 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
         */
         http.csrf(AbstractHttpConfigurer::disable);
+
+//       We are telling spring security that hey if any exception occurs then we have a custom exception handler .. use it
+          http.exceptionHandling(exception->
+                  exception.authenticationEntryPoint(unauthorizedHandler)
+                  );
+
+          http.addFilterBefore(authenticationJwtTokenFilter(),
+                  UsernamePasswordAuthenticationFilter.class);
+
 //        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+//        http.httpBasic(withDefaults());
         return http.build();
     }
 
+//   We have to use CommandLineRunner so that database will be created the moment the application gets loaded
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 =  User.withUsername("user1")
-//                .password("{noop}password1") //noop tells that password should be saved as plain text & should not be encoded. Not a good production practise
-                .password(passwordEncoder().encode("password1"))
-                .roles("USER")
-                .build();
-        UserDetails admin =  User.withUsername("admin")
-                .password(passwordEncoder().encode("demo"))
-                .roles("ADMIN")
-                .build();
+    public CommandLineRunner initData(UserDetailsService userDetailsService){
+        return args ->{
+          JdbcUserDetailsManager manager = (JdbcUserDetailsManager)  userDetailsService;
+          UserDetails user1 = User.withUsername("user1")
+                  .password(passwordEncoder().encode("password1"))
+                  .roles("USER")
+                  .build();
+          UserDetails admin = User.withUsername("admin")
+                  .password(passwordEncoder().encode("adminPass"))
+                  .roles("ADMIN")
+                  .build();
 
-        JdbcUserDetailsManager userDetailsManager =  new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.createUser(user1);
-        userDetailsManager.createUser(admin);
-        return userDetailsManager;
-//        return new InMemoryUserDetailsManager(user1, admin);
+          JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+          userDetailsManager.createUser(user1);
+          userDetailsManager.createUser(admin);
+        };
     }
+
+
+
+
+
+
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user1 =  User.withUsername("user1")
+////                .password("{noop}password1") //noop tells that password should be saved as plain text & should not be encoded. Not a good production practise
+//                .password(passwordEncoder().encode("password1"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin =  User.withUsername("admin")
+//                .password(passwordEncoder().encode("demo"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        JdbcUserDetailsManager userDetailsManager =  new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.createUser(user1);
+//        userDetailsManager.createUser(admin);
+//        return userDetailsManager;
+////        return new InMemoryUserDetailsManager(user1, admin);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+        return builder.getAuthenticationManager();
     }
 
 }
